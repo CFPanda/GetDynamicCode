b63dab1a6db3eade487c641f4d8e72858a34b5d1
duanchuanfen <admin@app.local>
2016-06-02 14:19:46 +0800

    Initial Commit

---
+++ GetDynamicCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetDynamicCode
+//
+//  Created by duanchuanfen on 16/6/2.
+//  Copyright © 2016年 duanchuanfen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
